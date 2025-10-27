f5100f225dbc32611b870844addc31ae92e97e1c
Igrm55 <158083336+Igrm55@users.noreply.github.com>
2025-10-26 21:15:46 -0300

    Delete backend directory

--- backend/src/middlewares/error.js
+++
@@ -1,6 +0,0 @@
-// Middleware de tratamento de erros
-module.exports = (err, req, res, next) => {
-  console.error(err);
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || 'Erro interno do servidor' });
-};
